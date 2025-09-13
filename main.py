16f2b190570931f434d474a2d7ef1e5125417876
Vitaliy Pozdnyakov <pozdnyakov.vitaliy@yandex.ru>
2025-09-13 20:34:38 +0800

    simplify CLI initialization

--- main.py
+++ main.py
@@ -5,10 +5,4 @@ from binconvfm.models.binconv import BinConvModule
 from binconvfm.models.lstm import LSTMModule
 from binconvfm.datamodules import DummyDataModule, SinDataModule, GiftEvalDataModule
 
-if __name__ == "__main__":
-    LightningCLI(
-        model_class=BaseLightningModule,
-        datamodule_class=LightningDataModule,
-        subclass_mode_model=True,
-        subclass_mode_data=True,
-    )
+cli = LightningCLI()
